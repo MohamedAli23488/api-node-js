dfeeb4def1e119b2d0fe1bdfe62e918fb8f9ee1e
mohamedmorsy <mohamed.ali.23488@gmail.com>
2025-11-10 16:17:56 +0200

    First commit

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -9,10 +9,17 @@ test.describe('User management API', () => {
         expect(response.status()).toBe(200);
         const responseBody = await response.text()
         expect(responseBody).toBe('[]');
+        console.log(responseBody)
     });
 
     test('find user: should return a user by ID', async ({ request }) => {
-
+    // first create user
+        const response = await request.post(`${baseURL}`);
+        const responseBody = await response.json()
+        const userId = responseBody.id;
+    // Find user by id
+        const responseUser = await request.get(`${baseURL}/${userId}`);
+        expect(responseUser.status()).toBe(200);
     });
 
     test('find user: should return 404 if user not found', async ({ request }) => {
@@ -20,16 +27,21 @@ test.describe('User management API', () => {
     });
 
     test('create user: should add a new user', async ({ request }) => {
+        const response = await request.post(`${baseURL}`);
+        expect(response.status()).toBe(201);
+        const responseBody = await response.json()
+        expect(responseBody).toBeDefined();
+        console.log(responseBody);
 
     });
-
-    test('delete user: should delete a user by ID', async ({ request }) => {
-
-    });
-
-    test('delete user: should return 404 if user not found', async ({ request }) => {
-
-    });
-
-
+//
+//     test('delete user: should delete a user by ID', async ({ request }) => {
+//
+//     });
+//
+//     test('delete user: should return 404 if user not found', async ({ request }) => {
+//
+//     });
+//
+//
 });
